3860f6857fb17770383f36d43afdd73f6d05dbd5
Jisb02 <noooop1234@naver.com>
2024-11-29 16:45:44 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,9 @@ st.write("아래 버튼을 눌러 CCTV 영상을 확인하세요.")
 
 # 버튼과 결과 화면
 if st.button("3층 복도 CCTV"):
-    st.image("CCTV.JPG", caption="3층 복도 CCTV")
+    st.image("cctv/CCTV.JPG", caption="3층 복도 CCTV")
     st.write("고장 사유: 파손")
 
 if st.button("인문대 엘리베이터 CCTV"):
-    st.image("CCTV.JPG", caption="인문대 엘리베이터 CCTV")
+    st.image("cctv/CCTV.JPG", caption="인문대 엘리베이터 CCTV")
     st.write("고장 사유: 파손")
