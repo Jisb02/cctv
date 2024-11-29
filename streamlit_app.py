c8a02df14c376c1d9368d6483a561fbdb197e11b
Jisb02 <noooop1234@naver.com>
2024-11-29 16:42:54 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# CCTV 페이지
+st.title("CCTV 모니터링")
+st.write("아래 버튼을 눌러 CCTV 영상을 확인하세요.")
+
+# 버튼과 결과 화면
+if st.button("3층 복도 CCTV"):
+    st.image("CCTV.JPG", caption="3층 복도 CCTV")
+    st.write("고장 사유: 파손")
+
+if st.button("인문대 엘리베이터 CCTV"):
+    st.image("CCTV.JPG", caption="인문대 엘리베이터 CCTV")
+    st.write("고장 사유: 파손")
